f575440a0c2ff48f97ca01f5febd786d7fffdf8c
JULIO CESAR AZOCAR MANSILLA <zf701299@tc2.tech>
2022-06-20 16:25:07 +0200

    Cliente con Get y post pruebas en project de services

--- src/main/java/com/devpredator/projectwebservicesconsumer/client/EmpleadoWSClient.java
+++ src/main/java/com/devpredator/projectwebservicesconsumer/client/EmpleadoWSClient.java
@@ -1,5 +1,87 @@
 package com.devpredator.projectwebservicesconsumer.client;
 
+import java.time.LocalDateTime;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.ClientConfig;
+
+import com.devpredator.projectwebservicesconsumer.dto.EmpleadoDTO;
+
 public class EmpleadoWSClient {
 
+	
+	public static void main(String[] args ) {
+		
+		
+		//::::::::::::::::::::::::GET::::::::::::::::::::::::::::::
+		
+//				Client client = ClientBuilder.newClient();
+//				WebTarget webTarget = 
+//						client.target("http://localhost:8080/project-webservice/devpredator/empleadosWS")
+//						.path("buscarEmpleadoPorId").path("1");
+//		
+//				Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+//				Response response = invocationBuilder.get();
+//				
+//				if (response.getStatus() == 204) {
+//					System.out.println("Empleado no encontrado");
+//				}
+//				
+//				if (response.getStatus() == 200) {
+//					EmpleadoDTO empleadoDTO = response.readEntity(EmpleadoDTO.class);
+//					
+//					System.out.println("Nombre empleado : " + empleadoDTO.getNombre());
+//					System.out.println("Fecha empleado : " + empleadoDTO.getFechaCreacion());
+//					
+//					//Nombre empleado : julio
+//					//Fecha empleado : 2022-06-19T03:20:28.963657800
+//				}
+		
+		//::::::::::::::::::::::::POST::::::::::::::::::::::::::::::
+		
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = 
+				client.target("http://localhost:8080/project-webservice/devpredator/empleadosWS")
+				.path("guardar");
+		
+		
+		EmpleadoDTO empleadoDTO = new EmpleadoDTO();
+		empleadoDTO.setId(5);
+		empleadoDTO.setNombre("benja");
+		empleadoDTO.setFechaCreacion(LocalDateTime.now());
+		empleadoDTO.setApellidos("Azocar");
+		empleadoDTO.setEdad(42);
+		
+		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+		Response response = invocationBuilder.post(Entity.entity(empleadoDTO, MediaType.APPLICATION_JSON));
+		
+		//imprimir error que arroja server
+		if (response.getStatus() == 400) {
+		String error = response.readEntity(String.class);
+		System.out.println("Server error : " + error);
+		}
+		
+		if (response.getStatus() != 200) {
+		System.out.println("Empleado no guardado");
+		}
+	
+		if (response.getStatus() == 200) {
+
+			System.out.println("Nombre empleado : " + empleadoDTO.getNombre());
+			System.out.println("Fecha empleado : " + empleadoDTO.getFechaCreacion());
+			
+			//Nombre empleado : julio
+			//Fecha empleado : 2022-06-19T03:20:28.963657800
+		}
+		
+		
+	}
+	
 }
--- src/main/java/com/devpredator/projectwebservicesconsumer/dto/EmpleadoDTO.java
+++ src/main/java/com/devpredator/projectwebservicesconsumer/dto/EmpleadoDTO.java
@@ -1,5 +1,49 @@
 package com.devpredator.projectwebservicesconsumer.dto;
 
-public class EmpleadoDTO {
+import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+
+public class EmpleadoDTO {
+	private long id;
+	private String Nombre;
+	private String Apellidos;
+	private long Edad;
+	
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	private LocalDateTime fechaCreacion;
+	
+	//
+	public LocalDateTime getFechaCreacion() {
+		return fechaCreacion;
+	}
+	public void setFechaCreacion(LocalDateTime fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellidos() {
+		return Apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		Apellidos = apellidos;
+	}
+	public long getEdad() {
+		return Edad;
+	}
+	public void setEdad(long edad) {
+		Edad = edad;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
 }
