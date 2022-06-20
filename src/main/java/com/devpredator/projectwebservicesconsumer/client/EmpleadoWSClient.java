31b5e0a3412cadb795a522e9119bbca7f34d2011
JULIO CESAR AZOCAR MANSILLA <zf701299@tc2.tech>
2022-06-20 18:29:45 +0200

    test con aws consumiendo project webservice

--- src/main/java/com/devpredator/projectwebservicesconsumer/client/EmpleadoWSClient.java
+++ src/main/java/com/devpredator/projectwebservicesconsumer/client/EmpleadoWSClient.java
@@ -16,70 +16,75 @@ import com.devpredator.projectwebservicesconsumer.dto.EmpleadoDTO;
 
 public class EmpleadoWSClient {
 
+	private static String urlAWS = "http://projectwebservice-env.eba-wi8swgqt.us-east-1.elasticbeanstalk.com/devpredator/empleadosWS";
+
+	///devpredator/empleadosWS/buscarEmpleadoPorId/1
+	
+	private static String urlLOCAL="http://localhost:8080/project-webservice/devpredator/empleadosWS";
 	
 	public static void main(String[] args ) {
 		
 		
 		//::::::::::::::::::::::::GET::::::::::::::::::::::::::::::
 		
-//				Client client = ClientBuilder.newClient();
-//				WebTarget webTarget = 
-//						client.target("http://localhost:8080/project-webservice/devpredator/empleadosWS")
-//						.path("buscarEmpleadoPorId").path("1");
-//		
-//				Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
-//				Response response = invocationBuilder.get();
-//				
-//				if (response.getStatus() == 204) {
-//					System.out.println("Empleado no encontrado");
-//				}
-//				
-//				if (response.getStatus() == 200) {
-//					EmpleadoDTO empleadoDTO = response.readEntity(EmpleadoDTO.class);
-//					
-//					System.out.println("Nombre empleado : " + empleadoDTO.getNombre());
-//					System.out.println("Fecha empleado : " + empleadoDTO.getFechaCreacion());
-//					
-//					//Nombre empleado : julio
-//					//Fecha empleado : 2022-06-19T03:20:28.963657800
-//				}
+				Client client = ClientBuilder.newClient();
+				WebTarget webTarget = 
+						client.target(urlAWS)
+						.path("buscarEmpleadoPorId").path("1");
+		
+				Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+				Response response = invocationBuilder.get();
+				
+				if (response.getStatus() == 204) {
+					System.out.println("Empleado no encontrado");
+				}
+				
+				if (response.getStatus() == 200) {
+					EmpleadoDTO empleadoDTO = response.readEntity(EmpleadoDTO.class);
+					
+					System.out.println("Nombre empleado : " + empleadoDTO.getNombre());
+					System.out.println("Fecha empleado : " + empleadoDTO.getFechaCreacion());
+					
+					//Nombre empleado : julio
+					//Fecha empleado : 2022-06-19T03:20:28.963657800
+				}
 		
 		//::::::::::::::::::::::::POST::::::::::::::::::::::::::::::
 		
-		Client client = ClientBuilder.newClient();
-		WebTarget webTarget = 
-				client.target("http://localhost:8080/project-webservice/devpredator/empleadosWS")
-				.path("guardar");
-		
-		
-		EmpleadoDTO empleadoDTO = new EmpleadoDTO();
-		empleadoDTO.setId(5);
-		empleadoDTO.setNombre("benja");
-		empleadoDTO.setFechaCreacion(LocalDateTime.now());
-		empleadoDTO.setApellidos("Azocar");
-		empleadoDTO.setEdad(42);
-		
-		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
-		Response response = invocationBuilder.post(Entity.entity(empleadoDTO, MediaType.APPLICATION_JSON));
-		
-		//imprimir error que arroja server
-		if (response.getStatus() == 400) {
-		String error = response.readEntity(String.class);
-		System.out.println("Server error : " + error);
-		}
-		
-		if (response.getStatus() != 200) {
-		System.out.println("Empleado no guardado");
-		}
-	
-		if (response.getStatus() == 200) {
-
-			System.out.println("Nombre empleado : " + empleadoDTO.getNombre());
-			System.out.println("Fecha empleado : " + empleadoDTO.getFechaCreacion());
-			
-			//Nombre empleado : julio
-			//Fecha empleado : 2022-06-19T03:20:28.963657800
-		}
+//		Client client = ClientBuilder.newClient();
+//		WebTarget webTarget = 
+//				client.target(urlAWS)
+//				.path("guardar");
+//		
+//		
+//		EmpleadoDTO empleadoDTO = new EmpleadoDTO();
+//		empleadoDTO.setId(5);
+//		empleadoDTO.setNombre("benja");
+//		empleadoDTO.setFechaCreacion(LocalDateTime.now());
+//		empleadoDTO.setApellidos("Azocar");
+//		empleadoDTO.setEdad(42);
+//		
+//		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+//		Response response = invocationBuilder.post(Entity.entity(empleadoDTO, MediaType.APPLICATION_JSON));
+//		
+//		//imprimir error que arroja server
+//		if (response.getStatus() == 400) {
+//		String error = response.readEntity(String.class);
+//		System.out.println("Server error : " + error);
+//		}
+//		
+//		if (response.getStatus() != 200) {
+//		System.out.println("Empleado no guardado");
+//		}
+//	
+//		if (response.getStatus() == 200) {
+//
+//			System.out.println("Nombre empleado : " + empleadoDTO.getNombre());
+//			System.out.println("Fecha empleado : " + empleadoDTO.getFechaCreacion());
+//			
+//			//Nombre empleado : julio
+//			//Fecha empleado : 2022-06-19T03:20:28.963657800
+//		}
 		
 		
 	}
